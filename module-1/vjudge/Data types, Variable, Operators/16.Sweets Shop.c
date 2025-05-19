65433149563c28ca74a316b5988f7cf2d3c32d75
Md Raihan Chowdhoury <mdr8077@gmail.com>
2025-05-19 21:32:33 +0600

    Update 16.Sweets Shop.c

--- module-1/vjudge/Data types, Variable, Operators/16.Sweets Shop.c
+++ module-1/vjudge/Data types, Variable, Operators/16.Sweets Shop.c
@@ -1,1 +1,10 @@
-// not provided
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	// your code goes here
+    int x, n;
+    scanf("%d%d", &x, &n);
+    int rem = x - n * 10;
+    printf("%d\n", rem / 20);
+    return 0;
+}
