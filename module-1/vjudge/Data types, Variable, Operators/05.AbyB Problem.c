2cbf10904f4280547e50e89ca08b23616e0422c2
Md Raihan Chowdhoury <mdr8077@gmail.com>
2025-05-19 21:27:27 +0600

    Update 05.AbyB Problem.c

--- module-1/vjudge/Data types, Variable, Operators/05.AbyB Problem.c
+++ module-1/vjudge/Data types, Variable, Operators/05.AbyB Problem.c
@@ -1,7 +1,7 @@
-#include <stdio.h> int main() {
-
-    int a, b;
-    scanf("%d %d", & a, & b);
-    printf("%d %d %.61f", a / b, a % b, (double) a / b);
+#include <stdio.h>
+int main () {
+	int a, b;
+	scanf("%d%d", &a, &b);
+	printf("%d %d %.6lf", a / b, a % b, (double) a / b);
     return 0;
 }
