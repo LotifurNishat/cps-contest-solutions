320ce82f440f65114120ed6c925f9fd3eddb3236
Md Raihan Chowdhoury <mdr8077@gmail.com>
2025-05-19 11:13:28 +0600

    Update 20.Leyland Number.c

--- module-1/vjudge/Data types, Variable, Operators/20.Leyland Number.c
+++ module-1/vjudge/Data types, Variable, Operators/20.Leyland Number.c
@@ -1,1 +1,10 @@
-// not provided
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int result = pow(a, b) + pow(b, a);
+    printf("%d\n", result);
+    return 0;
+}
