2631bbb9fda8891e329b31673302dc1405d55375
Md Raihan Chowdhoury <mdr8077@gmail.com>
2025-05-19 21:28:36 +0600

    Update 08.Attack.c

--- module-1/vjudge/Data types, Variable, Operators/08.Attack.c
+++ module-1/vjudge/Data types, Variable, Operators/08.Attack.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-
-int main() {
-    // A - x*B <= 0
-    // x * B >= A
-    // x >= A / B
-    // 5.1 -> 6 (ceiling)
-    // 5.1 -> 5 (floor)
-    // (27 + 4) / 5 = 6 (ceiling trick)
-    
-    long long a, b;
-    scanf("%lld %lld", &a, &b);
-    long long x = (a + b - 1) / b; // Ceiling division
-    printf("%lld\n", x);
+int main () {
+	// A - x*B <= 0
+	// x * B >= A
+	// x >= A / B
+	// 5.1 -> 6
+	// 5.1 -> 5
+	// 27 + 4 / 5 = 6
+	long long a, b;
+	scanf("%lld%lld", &a, &b);
+	long long x = (a + b - 1) / b;
+	printf("%lld\n", x);
     return 0;
 }
